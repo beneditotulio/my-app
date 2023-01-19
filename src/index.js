b0f0e4a1d5ca4f346445e043b0f637915e7ff40b
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2023-01-19 04:51:46 +0200

    Import files using the correct way

--- src/index.js
+++ src/index.js
@@ -4,6 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// <!-- Favicons -->
+// <link href='assets/img/favicon.png' rel='icon'>
+// <link href='assets/img/apple-touch-icon.png' rel='apple-touch-icon'>
+
+// <!-- Google Fonts -->
+// import 'https://fonts.gstatic.com'
+// import 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i'
+
+// <!-- Vendor CSS Files -->
+// import './assets/vendor/bootstrap/css/bootstrap.min.css'
+// import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
+// import './assets/vendor/boxicons/css/boxicons.min.css'
+// import './assets/vendor/quill/quill.snow.css'
+// import './assets/vendor/quill/quill.bubble.css'
+// import './assets/vendor/remixicon/remixicon.css'
+// import './assets/vendor/simple-datatables/style.css'
+
+// <!-- Template Main CSS File -->
+// import './assets/css/style.css'
+
+// <!-- Vendor JS Files -->
+// import './assets/vendor/apexcharts/apexcharts.min.js'
+// import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
+// import './assets/vendor/chart.js/chart.umd.js'
+// import './assets/vendor/echarts/echarts.min.js'
+// import './assets/vendor/quill/quill.min.js'
+// import './assets/vendor/simple-datatables/simple-datatables.js'
+// import './assets/vendor/tinymce/tinymce.min.js'
+// import './assets/vendor/php-email-form/validate.js'
+
+// <!-- Template Main JS File -->
+// import './assets/js/main.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
