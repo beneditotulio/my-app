d241e546d355b1daf13c4523dd64f20b594040f7
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2023-01-19 13:56:25 +0200

    mounting all component to build the UI

--- src/App.js
+++ src/App.js
@@ -4,32 +4,15 @@ import NavigationBar from './NavigationBar';
 import Footer from './Footer';
 import Header from './Header';
 import SideBar from './SideBar';
-import Main from './Main';
+import Content from './Content';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
         <div>
         <Header />
         <SideBar />
-        {/* <Main /> */}
-        {/* <NavigationBar />
-        <h1>Welcome to my React project</h1> */}
+        <NavigationBar />
+        {/* <Content /> */}
         <Footer />
       </div>
   );
